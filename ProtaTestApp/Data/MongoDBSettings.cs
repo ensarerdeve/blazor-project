1a8f113a0f691821489246f6c4c0770a4ded9ab6
ensarerdeve <ensarerdeve@gmail.com>
2024-07-07 14:48:30 +0300

    dasds

--- ProtaTestApp/Data/MongoDBSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace ProtaTestApp.Data
-{
-    public class MongoDBSettings
-    {
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-        public string UsersCollectionName { get; set; }
-    }
-}
